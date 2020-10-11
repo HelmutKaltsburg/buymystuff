e2c1d05efeda6ec20bfda082c9d21df899d796b8
HelmutKaltsburg <58108750+HelmutKaltsburg@users.noreply.github.com>
2020-10-11 15:49:05 +0300

    Added productList from store to homepage, loading spinner, error msg

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,31 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Product from "../Components/Product";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
+import { listProducts } from "../actions/productActions";
+import Message from "../Components/Message";
+import Loader from "../Components/Loader";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  // Extract data from state
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   // Loads data after the page loads
   useEffect(() => {
-    const fetchProducts = async () => {
-      // Use destructuring to extract data from response(res)
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>New Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message varian="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
