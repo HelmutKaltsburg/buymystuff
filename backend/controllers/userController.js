dd9107e9a1a528f44f1d0892f8987214747e1b8e
HelmutKaltsburg <58108750+HelmutKaltsburg@users.noreply.github.com>
2020-10-12 20:06:44 +0300

    Added JSONWebToken for authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import generateToken from "../util/generateToken.js";
 
 // @desc    Authenticate user & get token
 // @route   POST /api/users/login
@@ -13,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
