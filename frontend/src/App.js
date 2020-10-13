f174705f20b36dde48f499facbee6d8e7b182699
HelmutKaltsburg <58108750+HelmutKaltsburg@users.noreply.github.com>
2020-10-13 21:17:11 +0300

    User register reducer, action, and page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./Pages/HomePage";
 import ProductPage from "./Pages/ProductPage";
 import CartPage from "./Pages/CartPage";
 import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
@@ -15,6 +16,7 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
           <Route path="/product/:id" component={ProductPage} />
           <Route path="/cart/:id?" component={CartPage} />
           <Route path="/" component={HomePage} exact />
