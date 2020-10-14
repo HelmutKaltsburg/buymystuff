b088118319c81a4e6a6e3e45ac97ef198d213c25
HelmutKaltsburg <58108750+HelmutKaltsburg@users.noreply.github.com>
2020-10-14 17:53:52 +0300

    Added profile page and get user profile functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ProductPage from "./Pages/ProductPage";
 import CartPage from "./Pages/CartPage";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
+import ProfilePage from "./Pages/ProfilePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
@@ -17,6 +18,7 @@ function App() {
         <Container>
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
+          <Route path="/profile" component={ProfilePage} />
           <Route path="/product/:id" component={ProductPage} />
           <Route path="/cart/:id?" component={CartPage} />
           <Route path="/" component={HomePage} exact />
