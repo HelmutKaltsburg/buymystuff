68de43443038c27311327aa8e18792c85f513615
HelmutKaltsburg <58108750+HelmutKaltsburg@users.noreply.github.com>
2020-10-21 13:23:21 +0300

    Image upload backend configuration and endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -32,6 +34,12 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// Upload route
+app.use("api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Error middleware
 app.use(notFound);
